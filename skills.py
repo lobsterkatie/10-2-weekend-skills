1296c702edf60de5cab02b4e0e12c2787afb5395
Katie Byers <lobsterkatie@gmail.com>
2015-10-03 17:42:46 -0700

    find_common_items working

--- skills.py
+++ skills.py
@@ -2,6 +2,7 @@
 ADVANCED = False
 
 from string import punctuation
+from collections import Counter
 
 def count_unique(input_string):
     """Count unique words in a string.
@@ -80,7 +81,29 @@ def find_common_items(list1, list2):
 
     """
 
-    return []
+    # NOTE: not to be difficult, but the output you're describing doesn't make
+    # sense, IMHO... If one list has two copies of something and the second list
+    # has three copies, what they have in common is two copies, not three. I
+    # mean... right??
+
+    common_elements = []
+
+    # get a count of all the elements of each list
+    element_counts1 = Counter(list1)
+    element_counts2 = Counter(list2)
+
+    for element in element_counts1:
+        # if element (from first list) is also in second list
+        if element_counts2[element] != 0:
+            # then add that element the list of in-common elements
+            # here is where I think the output doesn't make sense
+            # changing 'max' to 'min,' however, would make it make sense
+            num_copies = max(element_counts1[element], 
+                             element_counts2[element])
+            for i in range(num_copies):
+                common_elements.append(element)
+
+    return common_elements
 
 
 def find_unique_common_items(list1, list2):
